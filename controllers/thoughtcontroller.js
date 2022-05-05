8cbd3905a0a711aadbb887ada4baa8478854cf07
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-05-05 09:55:42 -0500

    Controllers written.

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -11,19 +11,39 @@ module.exports = {
     getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
             .select("-__v")
-            .then((thought) => 
+            .then((thought) =>
                 !thought
-                ? res.status(404).json({ message: "There are no thoughts with that ID"})
-                : res.json(thought)
+                    ? res.status(404).json({ message: "There are no thoughts with that ID" })
+                    : res.json(thought)
             )
             .catch((err) => res.status(500).json(err));
     },
-    //Create a new though
-        //(don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
-    
+    //Create a new thought
+    //(don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            })
+    },
     //Update a thought by it's _id
-    
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.courseId },
+            { $set: req.body },
+        )
+    },
     //Delete a thought by it's _id
-
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId})
+            .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: "There are no thoughts with that ID" })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
 
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,68 @@
 const { User, Thought } = require("../models");
 const { ObjectId } = require("mongoose").Types;
 
-//Get all users
+async function getUsers (req, res){
+    try {
+       const getAll = await User.find()
+       return res.json(getAll) 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
 
 //Get one user by it's _id
+async function getOneUser (req, res){
+    try {
+        const getOne = await User.findById(req.params.userId)
+        return res.json(getOne)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json(error)
+    }
+}
 
 //Create a new user
+async function createUser (req, res){
+    try {
+        const create = await User.create(req.body)
+        return res.json(create)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
 
 //Update a User
+async function updateUserById (req, res){
+    try {
+        const updateUser = await User.updateOne(
+            { _id: req.params.userId},
+            req.body,
+            { runValidators: true, new: true }
+        )
+        return res.json(updateUser)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json(error)
+    }
+}
 
 //Remove one user by their _id
+async function removeUserById (req, res){
+    try {
+        const deleteUser = await User.deleteOne({_id: req.params.userId})
+        return res.json(deleteUser)
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json(error)
+    }
+}
+
+module.exports = {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUserById,
+    removeUserById
+}
